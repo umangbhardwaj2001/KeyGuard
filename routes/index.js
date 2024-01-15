4ae7492f2d28e07f3d8f69b8de67f5449cf0e982
Umang Bhardwaj <umangbhardwaj.ji@gmail.com>
2024-01-15 13:40:31 +0530

    add route to index

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  // const indexPath = path.join(__dirname, '../views/index.html')
+  // res.sendFile(indexPath);
+  res.render("index");
+});
+
+module.exports = router;
