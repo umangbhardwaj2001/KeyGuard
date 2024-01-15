569e702f8fae234d0a09601a0454c1dac1a7a8fc
Umang Bhardwaj <umangbhardwaj.ji@gmail.com>
2024-01-15 13:39:07 +0530

    add controller to interact with DB

---
+++ controllers/userController.js
@@ -0,0 +1,13 @@
+const User = require("../models/userModel");
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+module.exports = {
+  getAllUsers,
+};
